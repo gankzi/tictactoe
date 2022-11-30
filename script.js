bf0aec4bce358b0d93e440b37181a48c70b15c8a
Gankzi <hoonkang1024@gmail.com>
2022-11-30 13:22:32 -0800

    Add functionality to the game to be played by human
    Added functionality to Tic Tac Toe game to be played by humans.
    Game can be replayed again by a person once pressing restart.

--- script.js
+++ script.js
@@ -1,54 +1,32 @@
 const boxes = document.querySelectorAll([".box"]);
+const boxP = document.querySelectorAll([".box-p"]);
+const resultPage = document.querySelector(".result-page");
+const resultHeading = document.querySelector(".result-header");
 const classX = "X";
 const classO = "O";
-const box6 = document.querySelector("#box6-p");
-
+const winConditions = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
 let currentSign = classX;
 
-
 boxes.forEach((box) => box.addEventListener("click", displayMark, {once: true}));
-
-
-
-const Gameboard = () => {
-    const gameboard = [];
-
-}
-
-const Players = (sign) => {
-    this.sign = sign;
-
-    const getSign = () => {
-        return sign;
-    }
-
-    return {getSign};
-};
-
-function gameStart() {
-
-}
 
 function displayMark(event) {
     event.target.classList.add(currentSign);
     event.target.firstElementChild.textContent = currentSign;
-
-    currentSign = currentSign == classX ? classO : classX; 
-    
+   
     checkGameResult();
+    currentSign = currentSign == classX ? classO : classX; 
 }
 
 function checkGameResult() {
-    const winConditions = [
-        [0,1,2],
-        [3,4,5],
-        [6,7,8],
-        [0,3,6],
-        [1,4,7],
-        [2,5,8],
-        [0,4,8],
-        [2,4,6]
-    ];
 
     const xClassArray = [];
     const oClassArray = [];
@@ -63,15 +41,32 @@ function checkGameResult() {
         }
     };
 
-    console.log(xClassArray);
-    console.log(oClassArray);
+    function checkWin() {
+        if(winConditions.some(win => win.every(marked => {
+            return xClassArray.includes(marked);
+        }))) {
+            resultHeading.textContent = "X Wins!!!";
+            resultPage.setAttribute("id", "result");
+        } else if(winConditions.some(win => win.every(marked => {
+            return oClassArray.includes(marked);
+        }))) {
+            resultHeading.textContent = "O Wins!!!";
+            resultPage.setAttribute("id", "result");
+        } else if ((xClassArray.length + oClassArray.length) == 9) {
+            resultHeading.textContent = "Draw!!!";
+            resultPage.setAttribute("id", "result");
+        }
 
-    if(winConditions.some(x => x == xClassArray)) {
-        console.log("X Player Wins");
-    } else if (winConditions.includes(oClassArray)) {
-        console.log("O Player Wins");
-    }
+       };
+   
+       checkWin();
+};
 
+function restart() {
+    currentSign = classX;
+    resultPage.removeAttribute("id");
+    boxes.forEach((box) => box.classList.remove("O"));
+    boxes.forEach((box) => box.classList.remove("X"));
+    boxP.forEach((p) => p.textContent = "");
+    boxes.forEach((box) => box.addEventListener("click", displayMark, {once: true}));
 }
-
-console.log(boxes);
